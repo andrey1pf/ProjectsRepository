68543947fed821390073e1099394e4a2f89060e5
andrey1pf <goncharenkoandrey8@gmail.com>
2021-04-07 14:00:51 +0300

    --

--- Durak/WindowsFormsApp1/GameForm.Designer.cs
+++ Durak/WindowsFormsApp1/GameForm.Designer.cs
@@ -31,10 +31,10 @@ namespace WindowsFormsApp1
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
             this.label1 = new System.Windows.Forms.Label();
-            this.buttonYes = new System.Windows.Forms.Button();
             this.buttonNo = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.buttonYes = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -47,17 +47,6 @@ namespace WindowsFormsApp1
             this.label1.TabIndex = 0;
             this.label1.Text = "Ты дурак?";
             // 
-            // buttonYes
-            // 
-            this.buttonYes.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonYes.Location = new System.Drawing.Point(77, 324);
-            this.buttonYes.Name = "buttonYes";
-            this.buttonYes.Size = new System.Drawing.Size(254, 134);
-            this.buttonYes.TabIndex = 0;
-            this.buttonYes.TabStop = false;
-            this.buttonYes.Text = "Да";
-            this.buttonYes.UseVisualStyleBackColor = true;
-            // 
             // buttonNo
             // 
             this.buttonNo.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -68,11 +57,11 @@ namespace WindowsFormsApp1
             this.buttonNo.TabStop = false;
             this.buttonNo.Text = "Нет";
             this.buttonNo.UseVisualStyleBackColor = true;
-            this.buttonNo.Click += new System.EventHandler(this.button2_Click);
+            this.buttonNo.Click += new System.EventHandler(this.buttonNo_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(838, 32);
+            this.textBox1.Location = new System.Drawing.Point(825, 32);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
@@ -82,21 +71,33 @@ namespace WindowsFormsApp1
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(668, 9);
+            this.label2.Location = new System.Drawing.Point(646, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(270, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Попробуй ввести свои читы";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonYes
+            // 
+            this.buttonYes.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonYes.Location = new System.Drawing.Point(109, 324);
+            this.buttonYes.Name = "buttonYes";
+            this.buttonYes.Size = new System.Drawing.Size(254, 134);
+            this.buttonYes.TabIndex = 3;
+            this.buttonYes.Text = "Да";
+            this.buttonYes.UseVisualStyleBackColor = true;
+            this.buttonYes.Click += new System.EventHandler(this.buttonYes_Click);
             // 
             // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(950, 603);
+            this.Controls.Add(this.buttonYes);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.buttonNo);
-            this.Controls.Add(this.buttonYes);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimizeBox = false;
@@ -112,10 +113,10 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button buttonYes;
         private System.Windows.Forms.Button buttonNo;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonYes;
     }
 }
 
--- Durak/WindowsFormsApp1/GameForm.cs
+++ Durak/WindowsFormsApp1/GameForm.cs
@@ -15,9 +15,20 @@ namespace WindowsFormsApp1
         bool CheckFirstStep = false;
         int moveX, moveY, CursorX, CursorY;
         string cheat;
+        bool superimposition = false;
         public GameForm()
         {
             InitializeComponent();
+        }
+
+        private void buttonYes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Попробуй еще раз подумать", "Ты же не дурак");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -25,12 +36,9 @@ namespace WindowsFormsApp1
             cheat = textBox1.Text;
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Попробуй еще раз подумать", "Ты же не дурак");
-        }
 
-        private void button2_Click(object sender, EventArgs e)
+
+        private void buttonNo_Click(object sender, EventArgs e)
         {
             MessageBox.Show("У тебя получилось, но зачем было тратить свое время?", "Ошибка!");
         }
@@ -39,84 +47,116 @@ namespace WindowsFormsApp1
         {
             if (cheat == "AIWPRTON")
             {
+                buttonNo.Enabled = true;
                 return;
             }
             if (CheckFirstStep == false)
             {
-                moveX = (buttonNo.Width / 5);
-                moveY = (buttonNo.Height / 5);
+                moveX = (buttonNo.Width / 3);
+                moveY = (buttonNo.Height / 3);
                 CursorX = e.X;
                 CursorY = e.Y;
             }
+            superimposition = false;
+            buttonNo.Enabled = false;
             // правый нижний угол
-            if (CursorX >= buttonNo.Location.X - 2 && CursorX <= buttonNo.Location.X + (buttonNo.Width / 3) - 2)
+            if (CursorX >= buttonNo.Location.X - 20 && CursorX <= buttonNo.Location.X + (buttonNo.Width / 3) - 20)
             {
-                if (CursorY >= buttonNo.Location.Y - 2 && CursorY <= buttonNo.Location.Y + (buttonNo.Height / 3) - 2)
+                if (CursorY >= buttonNo.Location.Y - 20 && CursorY <= buttonNo.Location.Y + (buttonNo.Height / 3) - 20)
                 {
                     buttonNo.Location = new Point(buttonNo.Location.X + moveX, buttonNo.Location.Y + moveY);
+                    superimposition = true;
                 }
             }
             // вниз
-            if (CursorX >= buttonNo.Location.X + (buttonNo.Width / 3) - 2 && CursorX <= buttonNo.Location.X + 2 * (buttonNo.Width / 3) - 2)
+            if (CursorX >= buttonNo.Location.X + (buttonNo.Width / 3) - 20 && CursorX <= buttonNo.Location.X + 2 * (buttonNo.Width / 3) - 20)
             {
-                if (CursorY >= buttonNo.Location.Y - 2 && CursorY <= buttonNo.Location.Y + (buttonNo.Height / 3) - 2)
+                if (CursorY >= buttonNo.Location.Y - 20 && CursorY <= buttonNo.Location.Y + (buttonNo.Height / 3) - 20)
                 {
                     buttonNo.Location = new Point(buttonNo.Location.X, buttonNo.Location.Y + moveY);
+                    superimposition = true;
                 }
             }
             // в левый нижний угол
-            if (CursorX >= buttonNo.Location.X + 2 * (buttonNo.Width / 3) + 2 && CursorX <= buttonNo.Location.X + buttonNo.Width + 2)
+            if (CursorX >= buttonNo.Location.X + 2 * (buttonNo.Width / 3) + 20 && CursorX <= buttonNo.Location.X + buttonNo.Width + 20)
             {
-                if (CursorY >= buttonNo.Location.Y - 2 && CursorY <= buttonNo.Location.Y + (buttonNo.Height / 3) - 2)
+                if (CursorY >= buttonNo.Location.Y - 20 && CursorY <= buttonNo.Location.Y + (buttonNo.Height / 3) - 20)
                 {
                     buttonNo.Location = new Point(buttonNo.Location.X - moveX, buttonNo.Location.Y + moveY);
+                    superimposition = true;
                 }
             }
             // влево
-            if (CursorX >= buttonNo.Location.X + 2 * (buttonNo.Width / 3) + 2 && CursorX <= buttonNo.Location.X + buttonNo.Width + 2)
+            if (CursorX >= buttonNo.Location.X + 2 * (buttonNo.Width / 3) + 20 && CursorX <= buttonNo.Location.X + buttonNo.Width + 20)
             {
-                if (CursorY >= buttonNo.Location.Y + (buttonNo.Height / 3) - 2 && CursorY <= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) + 2)
+                if (CursorY >= buttonNo.Location.Y + (buttonNo.Height / 3) - 20 && CursorY <= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) + 20)
                 {
                     buttonNo.Location = new Point(buttonNo.Location.X - moveX, buttonNo.Location.Y);
+                    superimposition = true;
                 }
             }
             // в левый верхний угол
-            if (CursorX >= buttonNo.Location.X + 2 * (buttonNo.Width / 3) + 2 && CursorX <= buttonNo.Location.X + buttonNo.Width + 2)
+            if (CursorX >= buttonNo.Location.X + 2 * (buttonNo.Width / 3) + 20 && CursorX <= buttonNo.Location.X + buttonNo.Width + 20)
             {
-                if (CursorY >= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) - 2 && CursorY <= buttonNo.Location.Y + buttonNo.Height + 2)
+                if (CursorY >= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) - 20 && CursorY <= buttonNo.Location.Y + buttonNo.Height + 20)
                 {
                     buttonNo.Location = new Point(buttonNo.Location.X - moveX, buttonNo.Location.Y - moveY);
+                    superimposition = true;
                 }
             }
             // вверх
-            if (CursorX >= buttonNo.Location.X + (buttonNo.Width / 3) - 2 && CursorX <= buttonNo.Location.X + 2 * (buttonNo.Width / 3) - 2)
+            if (CursorX >= buttonNo.Location.X + (buttonNo.Width / 3) - 20 && CursorX <= buttonNo.Location.X + 2 * (buttonNo.Width / 3) - 20)
             {
-                if (CursorY >= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) - 2 && CursorY <= buttonNo.Location.Y + buttonNo.Height + 2)
+                if (CursorY >= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) - 20 && CursorY <= buttonNo.Location.Y + buttonNo.Height + 20)
                 {
                     buttonNo.Location = new Point(buttonNo.Location.X, buttonNo.Location.Y - moveY);
+                    superimposition = true;
                 }
             }
             // в правый верхний угол
-            if (CursorX >= buttonNo.Location.X - 2 && CursorX <= buttonNo.Location.X + (buttonNo.Width / 3) - 2)
+            if (CursorX >= buttonNo.Location.X - 20 && CursorX <= buttonNo.Location.X + (buttonNo.Width / 3) - 20)
             {
-                if (CursorY >= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) - 2 && CursorY <= buttonNo.Location.Y + buttonNo.Height + 2)
+                if (CursorY >= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) - 20 && CursorY <= buttonNo.Location.Y + buttonNo.Height + 20)
                 {
                     buttonNo.Location = new Point(buttonNo.Location.X + moveX, buttonNo.Location.Y - moveY);
+                    superimposition = true;
                 }
             }
             // вправо
-            if (CursorX >= buttonNo.Location.X - 2 && CursorX <= buttonNo.Location.X + (buttonNo.Width / 3) - 2)
+            if (CursorX >= buttonNo.Location.X - 20 && CursorX <= buttonNo.Location.X + (buttonNo.Width / 3) - 20)
             {
-                if (CursorY >= buttonNo.Location.Y + (buttonNo.Height / 3) - 2 && CursorY <= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) - 2)
+                if (CursorY >= buttonNo.Location.Y + (buttonNo.Height / 3) - 20 && CursorY <= buttonNo.Location.Y + 2 * (buttonNo.Height / 3) - 20)
                 {
                     buttonNo.Location = new Point(buttonNo.Location.X + moveX, buttonNo.Location.Y);
+                    superimposition = true;
                 }
             }
 
-            // верхняя граница
-            if (buttonNo.Location.X >= 0 && buttonNo.Location.X <= this.ClientSize.Width && buttonNo.Location.Y >= 0 && buttonNo.Location.Y <= moveY)
+
+
+            Random random = new Random();
+
+            if (CursorX >= buttonNo.Location.X - 1 && CursorX <= buttonNo.Location.X + buttonNo.Width + 1)
             {
-                buttonNo.Location = new Point(buttonNo.Location.X, buttonNo.Location.Y + 200);          
+                if (CursorY >= buttonNo.Location.Y - 1 && CursorY <= buttonNo.Location.Y + buttonNo.Height + 1)
+                {
+                    int x = random.Next(0, this.ClientSize.Width);
+                    int y = random.Next(0, this.ClientSize.Height);
+                    buttonNo.Location = new Point(x, y);
+                }
+            }
+
+            if (superimposition = false)
+            {
+                int x = buttonNo.Location.X + 150;
+                int y = buttonNo.Location.Y + 150;
+                buttonNo.Location = new Point(x, y);
+            }
+
+            // верхняя граница
+            if (buttonNo.Location.X >= 0 && buttonNo.Location.X <= .Location.Y >= 0 && buttonNo.Location.Y <= moveY)
+            {
+                buttonNo.Location = new Point(buttonNo.Location.X, buttonNo.Location.Y + 200);
             }
             // левая граница
             if (buttonNo.Location.X >= 0 && buttonNo.Location.X <= moveX && buttonNo.Location.Y >= 0 && buttonNo.Location.Y <= this.ClientSize.Height)
@@ -135,9 +175,8 @@ namespace WindowsFormsApp1
                 buttonNo.Location = new Point(buttonNo.Location.X, buttonNo.Location.Y - 200);
             }
             // если кнопки совпадут, то buttonNo переместиться
-            if (buttonYes.Location.X == buttonNo.Location.X && buttonYes.Location.Y == buttonNo.Location.Y) { 
-                buttonNo.Location = new Point(buttonNo.Location.X + 150, buttonNo.Location.Y - 100); 
-            }
+
+
         }
     }
 }
